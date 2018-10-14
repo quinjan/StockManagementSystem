18a96d1a31a934ebb09927a03f3cb905abbba275
Quinjan Robert Ocampo <quinjanrobertocampo@gmail.com>
2018-10-15 04:51:43 +0800

    Added main.py

---
+++ Code/GUI/PythonFile/main.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("../../")
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from BusinessLogic.items import Items
+from BusinessLogic.users import Users
+from GUI.PythonFile.mainWindow import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(self.size())
+        self.statusBar().setSizeGripEnabled(False)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.lineEdit2.setValidator(QtGui.QIntValidator())
+
