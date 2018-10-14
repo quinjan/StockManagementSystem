d8af064b156a8f139e5c2b150bf3bfbae7970102
Ian Gabrielle Bautista <42402512+iangabrielle@users.noreply.github.com>
2018-10-15 04:27:50 +0800

    Update DataAccess.py

--- Code/DataLogic/DataAccess.py
+++ Code/DataLogic/DataAccess.py
@@ -6,8 +6,19 @@ class AccessData(object):
     def __init__(self):
         self.connection = sqlite3.connect('../Database/MainDB.db')
         self.dl = self.connection.cursor()
-
-    def readUser(self, username, password):
-        self.dl.execute('SELECT * FROM "Users"')
-        items = self.dl.fetchall()
-        return items
+        
+    def readUser(self):
+        self.dl.execute('SELECT * FROM Users')
+        return self.dl.fetchall()
+        
+    def readItems(self):
+        self.dl.execute('SELECT * FROM Items')
+        return self.dl.fetchall()
+    
+    def readKitchen(self):
+        self.items = self.dl.execute('SELECT * FROM Items WHERE Type = "Kitchen" ')
+        return self.items
+    
+    def readCold(self):
+        self.items = self.dl.execute('SELECT * FROM Items WHERE Type = "Cold" ')
+        return self.items
