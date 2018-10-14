1653e36b3158dbdc877ff595dd15f530fee1a091
Ian Gabrielle Bautista <42402512+iangabrielle@users.noreply.github.com>
2018-10-15 04:22:11 +0800

    Update users.py

--- Code/BusinessLogic/users.py
+++ Code/BusinessLogic/users.py
@@ -6,7 +6,25 @@ class Users(object):
     
     def __init__(self):
         self.dataReader = AccessData()
-        self.userList = []
+        self.Username = []
+        self.Password = []
+        self.Firstname = []
+        self.Lastname = []
+        self.UserID = []
+        
         
     def readUsers(self):
-        data = 
+        data = self.dataReader.readUser()
+        for data_out in data:
+            self.UserID.append(data_out[0])
+            self.Firstname.append(data_out[1])
+            self.Lastname.append(data_out[2])
+            self.Username.append(data_out[3])
+            self.Password.append(data_out[4])
+    
+    def verifyUser(self, username, password):
+        if username in self.Username:
+            if password == self.Password[self.Username.index(username)]:
+                return True
+        else:
+            return False
