19626155f9f8301360232491b5914812257638b8
Ryan Tumangday <ryantumangday@Ryans-MacBook-Air.local>
2018-10-15 13:32:39 +0800

    modified dataaccess.py

--- Code/DataLogic/DataAccess.py
+++ Code/DataLogic/DataAccess.py
@@ -22,3 +22,45 @@ class AccessData(object):
     def readCold(self):
         self.items = self.dl.execute('SELECT * FROM Items WHERE Type = "Cold" ')
         return self.items
+        def readHard(self):
+        self.items = self.dl.execute('SELECT * FROM Items WHERE Type = "Hard" ')
+        return self.items
+    
+    def writeKitchen(self,name,price,tayp,stock):
+        self.dl.execute("INSERT INTO Items (Name,Price,Type,Stock,Sales) values(?,?,?,?,0)",(name,price,tayp,stock))
+        self.dl.execute("COMMIT")
+
+    def countKitchen(self):
+        self.dl.execute('SELECT COUNT(FoodID) FROM Items WHERE Type = "Kitchen"')
+        self.count = self.dl.fetchone()[0]
+        return self.count
+    
+    def countCold(self):
+        self.dl.execute('SELECT COUNT(FoodID) FROM Items WHERE Type = "Cold"')
+        self.count = self.dl.fetchone()[0]
+        return self.count
+    
+    def countHard(self):
+        self.dl.execute('SELECT COUNT(FoodID) FROM Items WHERE Type = "Hard"')
+        self.count = self.dl.fetchone()[0]
+        return self.count
+    
+    def deleteItem(self, index):
+        self.dl.execute('DELETE FROM Items WHERE FoodID = %s' % (index))
+        self.dl.execute("COMMIT")
+        
+    def readKitchenSales(self):
+        self.items = self.dl.execute('SELECT Name, Sales, Stock FROM Items WHERE Type = "Kitchen" ')
+        return self.items
+    
+    def readColdSales(self):
+        self.items = self.dl.execute('SELECT Name, Sales, Stock FROM Items WHERE Type = "Cold" ')
+        return self.items
+    
+    def readHardSales(self):
+        self.items = self.dl.execute('SELECT Name, Sales, Stock FROM Items WHERE Type = "Hard" ')
+        return self.items
+    
+    def writeNewSale(self, value, stock, name):
+        self.dl.execute("UPDATE Items SET Sales = ?, Stock = ? WHERE Name = ?",(value, stock, name))
+        self.dl.execute("COMMIT")
